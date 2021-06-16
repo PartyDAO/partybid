9c9d5617bbb436afe89dd06b442c1a5013bbd5e5
anna-caroll <anna.s.carroll@gmail.com>
2021-06-16 13:19:18 -0700

    PartyBid Contracts

---
+++ contracts/PartyBidFactory.sol
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.4;
+
+import {PartyBidProxy} from "./PartyBidProxy.sol";
+import {PartyBid} from "./PartyBid.sol";
+
+/**
+ * @title PartyBid Factory
+ * @author Anna Carroll
+ * forked from MirrorXYZ CrowdfundFactory https://github.com/mirror-xyz/crowdfund/blob/main/contracts/CrowdfundFactory.sol
+ */
+contract PartyBidFactory {
+    //======== Events ========
+
+    event PartyBidDeployed(
+        address partyBidProxy,
+        address creator,
+        address nftContract,
+        uint256 tokenId,
+        address marketWrapper,
+        uint256 auctionId,
+        string name,
+        string symbol
+    );
+
+    //======== Immutable storage =========
+
+    address public immutable logic;
+    address public immutable partyDAOMultisig;
+    address public immutable tokenVaultFactory;
+    address public immutable weth;
+
+    //======== Constructor =========
+
+    constructor(
+        address _partyDAOMultisig,
+        address _tokenVaultFactory,
+        address _weth
+    ) {
+        partyDAOMultisig = _partyDAOMultisig;
+        tokenVaultFactory = _tokenVaultFactory;
+        weth = _weth;
+        // deploy logic contract
+        logic = address(
+            new PartyBid(_partyDAOMultisig, _tokenVaultFactory, _weth)
+        );
+    }
+
+    //======== Deploy function =========
+
+    function startParty(
+        address _marketWrapper,
+        address _nftContract,
+        uint256 _tokenId,
+        uint256 _auctionId,
+        string memory _name,
+        string memory _symbol
+    ) external returns (address partyBidProxy) {
+        partyBidProxy = address(
+            new PartyBidProxy(
+                logic,
+                _marketWrapper,
+                _nftContract,
+                _tokenId,
+                _auctionId,
+                _name,
+                _symbol
+            )
+        );
+
+        emit PartyBidDeployed(
+            partyBidProxy,
+            msg.sender,
+            _nftContract,
+            _tokenId,
+            _marketWrapper,
+            _auctionId,
+            _name,
+            _symbol
+        );
+    }
+}
---
+++ contracts/market-wrapper/FoundationMarketWrapper.sol
@@ -0,0 +1,127 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.4;
+
+// ============ External Imports ============
+import {IFoundationMarket} from "../external/interfaces/IFoundationMarket.sol";
+
+// ============ Internal Imports ============
+import {IMarketWrapper} from "./IMarketWrapper.sol";
+
+/**
+ * @title FoundationMarketWrapper
+ * @author Anna Carroll
+ * @notice MarketWrapper contract implementing IMarketWrapper interface
+ * according to the logic of Foundation's NFT Market
+ * Original Foundation NFT Market code: https://etherscan.io/address/0xa7d94560dbd814af316dd96fde78b9136a977d1c#code
+ */
+contract FoundationMarketWrapper is IMarketWrapper {
+    // ============ Internal Immutables ============
+
+    IFoundationMarket internal immutable market;
+
+    // ======== Constructor =========
+
+    constructor(address _foundationMarket) {
+        market = IFoundationMarket(_foundationMarket);
+    }
+
+    // ======== External Functions =========
+
+    /**
+     * @notice Determine whether there is an existing auction
+     * for this token on the market
+     * @return TRUE if the auction exists
+     */
+    function auctionExists(uint256 auctionId)
+        public
+        view
+        override
+        returns (bool)
+    {
+        // line 219 of NFTMarketReserveAuction, logic within placeBid() function (not exposed publicly)
+        IFoundationMarket.ReserveAuction memory _auction =
+            market.getReserveAuction(auctionId);
+        return _auction.amount != 0;
+    }
+
+    /**
+     * @notice Determine whether the given auctionId is
+     * an auction for the tokenId + nftContract
+     * @return TRUE if the auctionId matches the tokenId + nftContract
+     */
+    function auctionIdMatchesToken(
+        uint256 auctionId,
+        address nftContract,
+        uint256 tokenId
+    ) public view override returns (bool) {
+        return auctionId == market.getReserveAuctionIdFor(nftContract, tokenId);
+    }
+
+    /**
+     * @notice Query the current highest bidder for this auction
+     * @return highest bidder
+     */
+    function getCurrentHighestBidder(uint256 auctionId)
+        external
+        view
+        override
+        returns (address)
+    {
+        // line 279 of NFTMarketReserveAuction, getMinBidAmount() function
+        IFoundationMarket.ReserveAuction memory _auction =
+            market.getReserveAuction(auctionId);
+        return _auction.bidder;
+    }
+
+    /**
+     * @notice Calculate the minimum next bid for this auction
+     * @return minimum bid amount
+     */
+    function getMinimumBid(uint256 auctionId)
+        external
+        view
+        override
+        returns (uint256)
+    {
+        // line 279 of NFTMarketReserveAuction, getMinBidAmount() function
+        return market.getMinBidAmount(auctionId);
+    }
+
+    /**
+     * @notice Submit bid to Market contract
+     */
+    function bid(uint256 auctionId, uint256 bidAmount) external override {
+        // line 217 of NFTMarketReserveAuction, placeBid() function
+        (bool success, ) =
+            address(market).call{value: bidAmount}(
+                abi.encodeWithSignature("placeBid(uint256)", auctionId)
+            );
+        require(success, "place bid failed");
+    }
+
+    /**
+     * @notice Determine whether the auction has been finalized
+     * @return TRUE if the auction has been finalized
+     */
+    function isFinalized(uint256 auctionId)
+        external
+        view
+        override
+        returns (bool)
+    {
+        // line 266 of NFTMarketReserveAuction,
+        // the auction is deleted at the end of the finalizeReserveAuction() function
+        // since we checked that the auction DID exist when we deployed the partyBid,
+        // if it no longer exists that means the auction has been finalized.
+        return !auctionExists(auctionId);
+    }
+
+    /**
+     * @notice Finalize the results of the auction
+     */
+    function finalize(uint256 auctionId) external override {
+        // line 260 of finalizeReserveAuction, finalizeReserveAuction() function
+        // will revert if auction has not started or still in progress
+        market.finalizeReserveAuction(auctionId);
+    }
+}
