b04fe8ec62ed7e7c6fd8f386bda2eaac248a9244
Anna Carroll <anna-carroll@users.noreply.github.com>
2021-06-23 21:12:21 +0200

    Deploy script improvements + fresh deploy (#10)
    * Fresh deploy with official Fractional
    
    * Fix deploy script
    
    * npm command for deploy script

--- deploy/index.js
+++ deploy/index.js
@@ -17,7 +17,7 @@ async function deployChain() {
     dotenv.config();
     const {CHAIN_NAME, RPC_ENDPOINT, DEPLOYER_PRIVATE_KEY} = process.env;
     // load config.json
-    const config = JSON.parse(fs.readFileSync(`./configs/${CHAIN_NAME}.json`));
+    const config = JSON.parse(fs.readFileSync(`./deploy/configs/${CHAIN_NAME}.json`));
     const {partyDAOMultisig, factionalArtERC721VaultFactory, weth, foundationMarket, zoraAuctionHouse} = config;
 
     console.log(`Deploying ${CHAIN_NAME}`);
@@ -50,11 +50,11 @@ async function deployChain() {
         chain: CHAIN_NAME,
         partyBidFactory: factory.address,
         marketWrappers: {
-            foundation: null,
+            foundation: foundationMarketWrapper.address,
             zora: zoraMarketWrapper.address
         }
     };
-    const directory = "./addresses";
+    const directory = "./deploy/addresses";
     const filename = `${directory}/${CHAIN_NAME}.json`;
     fs.mkdirSync(directory, { recursive: true });
     fs.writeFileSync(
