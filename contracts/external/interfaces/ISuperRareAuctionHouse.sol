e3e8082736619afc677798ef6e41fb75e7dfeffe
Zach Kolodny <kolodnyz@icloud.com>
2021-08-24 17:36:31 -0400

    added sr market wrapper

---
+++ contracts/external/interfaces/ISuperRareAuctionHouse.sol
@@ -0,0 +1,120 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.5;
+
+interface ISuperRareAuctionHouse {
+    /////////////////////////////////////////////////////////////////////////
+    // Constants
+    /////////////////////////////////////////////////////////////////////////
+
+    // Types of Auctions
+    bytes32 public constant COLDIE_AUCTION = "COLDIE_AUCTION";
+    bytes32 public constant SCHEDULED_AUCTION = "SCHEDULED_AUCTION";
+    bytes32 public constant NO_AUCTION = bytes32(0);
+
+    /////////////////////////////////////////////////////////////////////////
+    // Structs
+    /////////////////////////////////////////////////////////////////////////
+    // A reserve auction.
+    struct Auction {
+        address payable auctionCreator;
+        uint256 creationBlock;
+        uint256 lengthOfAuction;
+        uint256 startingBlock;
+        uint256 reservePrice;
+        uint256 minimumBid;
+        bytes32 auctionType;
+    }
+
+    // The active bid for a given token, contains the bidder, the marketplace fee at the time of the bid, and the amount of wei placed on the token
+    struct ActiveBid {
+        address payable bidder;
+        uint8 marketplaceFee;
+        uint256 amount;
+    }
+
+    /////////////////////////////////////////////////////////////////////////
+    // Relevant Variables
+    /////////////////////////////////////////////////////////////////////////
+    uint8 public minimumBidIncreasePercentage;
+
+    /////////////////////////////////////////////////////////////////////////
+    // bid
+    /////////////////////////////////////////////////////////////////////////
+    /**
+     * @dev Bid on artwork with an auction.
+     * Rules:
+     * - if auction creator is still owner, owner must have contract approved
+     * - There must be a running auction or a reserve price auction for the token
+     * - bid > 0
+     * - if startingBlock - block.number < auctionLengthExtension
+     * -    then auctionLength = Starting block - (currentBlock + extension)
+     * - Auction creator != bidder
+     * - bid >= minimum bid
+     * - bid >= reserve price
+     * - block.number < startingBlock + lengthOfAuction
+     * - bid > current bid
+     * - if previous bid then returned
+     * @param _contractAddress address of ERC721 contract.
+     * @param _tokenId uint256 id of the token.
+     * @param _amount uint256 Wei value of the bid.
+     */
+    function bid(address _contractAddress, uint256 _tokenId, uint256 _amount) 
+        external 
+        payable;
+
+    /////////////////////////////////////////////////////////////////////////
+    // settleAuction
+    /////////////////////////////////////////////////////////////////////////
+    /**
+    * @dev Settles the auction, transferring the auctioned token to the bidder and the bid to auction creator.
+    * Rules:
+    * - There must be an unsettled auction for the token
+    * - current bidder becomes new owner
+    * - auction creator gets paid
+    * - there is no longer an auction for the token
+    * @param _contractAddress address of ERC721 contract.
+    * @param _tokenId uint256 id of the token.
+    */
+    function settleAuction(address _contractAddress, uint256 _tokenId)
+        external;
+
+    /////////////////////////////////////////////////////////////////////////
+    // getAuctionDetails
+    /////////////////////////////////////////////////////////////////////////
+    /**
+    * @dev Get current auction details for a token
+    * Rules:
+    * - Return empty when there's no auction
+    * @param _contractAddress address of ERC721 contract.
+    * @param _tokenId uint256 id of the token.
+    */
+    function getAuctionDetails(address _contractAddress, uint256 _tokenId)
+        external
+        view
+        returns (
+            bytes32,
+            uint256,
+            address,
+            uint256,
+            uint256,
+            uint256,
+            uint256
+        );
+
+    /////////////////////////////////////////////////////////////////////////
+    // getCurrentBid
+    /////////////////////////////////////////////////////////////////////////
+    /**
+     * @dev Get the current bid
+     * Rules:
+     * - Return empty when there's no bid
+     * @param _contractAddress address of ERC721 contract.
+     * @param _tokenId uint256 id of the token.
+     */
+    function getCurrentBidAmount(address _contractAddress, uint256 _tokenId)
+        external
+        view
+        returns (address, uint256);
+
+    
+}
---
+++ contracts/market-wrapper/SuperRareMarketWrapper.sol
@@ -0,0 +1,179 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.5;
+
+// ============ External Imports ============
+import {ISuperRareAuctionHouse} from "../external/interfaces/ISuperRareAuctionHouse.sol";
+import { SafeMath } from "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+// ============ Internal Imports ============
+import {IMarketWrapper} from "./IMarketWrapper.sol";
+
+/**
+ * @title SuperRareMarketWrapper
+ * @author Zach Kolodny
+ * @notice MarketWrapper contract implementing IMarketWrapper interface
+ * according to the logic of SuperRare's NFT Auction House
+ * Original SuperRare NFT AuctionHouse code: https://github.com/pixura/pixura-contracts/blob/master/contracts/src/SuperRareAuctionHouse.sol
+ */
+contract SuperRareMarketWrapper is IMarketWrapper {
+    using SafeMath for uint256;
+
+    // ============ Structs ============
+    struct Token {
+        address contractAddress,
+        uint256 tokenId
+    }
+
+    // ============ Internal Immutables ============
+    ISuperRareAuctionHouse internal immutable auctionHouse;
+
+    // ============ Public Variables ============
+    uint256 public auctionIdTracker;
+    mapping(uint256 => Token) public auctions;
+
+    // ======== Constructor =========
+    constructor(address _superRareAuctionHouse) {
+        auctionHouse = ISuperRareAuctionHouse(_superRareAuctionHouse);
+    }
+
+    // ============ Public Mutable Storage ============
+    uint256 public nextAuctionId = 1;
+
+    // ======== External Functions =========
+
+    /**
+     * @notice SuperRare doesn't use auction Ids so this function 
+     * will always return true
+     * @return TRUE always
+     */
+    function auctionExists(uint256 _auctionId)
+        external
+        view
+        returns (bool)
+    {
+        return true;
+    }
+
+    /**
+     * @notice Determines whether an auction exists/is not finished
+     * since SuperRare doesn't use auctionIds
+     * @return TRUE if the auctionId matches the tokenId + nftContract
+     */
+    function auctionIdMatchesToken(
+        uint256 _auctionId,
+        address _contractAddress,
+        uint256 _tokenId
+    ) external view returns (bool)
+    {
+        ISuperRareAuctionHouse.Auction memory auction = 
+            auctionHouse.getAuctionDetails(_contractAddress, _tokenId);
+
+        auctions[nextAuctionId] = Token(_contractAddress, _tokenId);
+
+        nextAuctionId++;
+
+        require(
+            auction.auctionType != NO_AUCTION,
+            "bid::Must have existing auction"
+        );
+
+        require(
+            auction.startingBlock <= block.number,
+            "bid::Must have a running auction or pending coldie auction"
+        );
+
+        return true;
+    }
+
+    /**
+     * @notice Calculate the minimum next bid for this auction
+     * @return minimum bid amount
+     */
+    function getMinimumBid(uint256 _auctionId) 
+        external 
+        view 
+        returns (uint256) 
+    {
+        Token token = auctions[_auctionId];
+
+        reqiure(token.tokenId != 0, "getMinimumBid::Auction doesnt exist for given auctionId");
+
+        (_, uint256 currentBid) = auctionHouse.getCurrentBidAmount(
+            token.contractAddress, 
+            token.tokenId
+        );
+        uint256 minBidIncrease = auctionHouse.minimumBidIncreasePercentage;
+
+        uint256 amount = currentBid.add(
+            currentBid.mul(minBidIncrease)
+                .div(100)
+        );
+
+        return amount.add(
+            auctionHouse.iMarketSettings.calculateMarketplaceFee(currentBid)
+        );
+    }
+
+    /**
+     * @notice Query the current highest bidder for this auction
+     * @return highest bidder
+     */
+    function getCurrentHighestBidder(uint256 auctionId)
+        external
+        view
+        returns (address) 
+    {
+        Token token = auctions[_auctionId];
+
+        reqiure(token.tokenId != 0, "getCurrentHighestBidder::Auction doesnt exist for given auctionId");
+
+        (address highestBidder, _) = auctionHouse.getCurrentBidAmount(token.contractAddress, tokenId);
+
+        return highestBidder;
+    }
+
+    /**
+     * @notice Submit bid to Market contract
+     */
+    function bid(uint256 auctionId, uint256 bidAmount) external {
+        Token token = auctions[_auctionId];
+
+        (bool success, bytes memory returnData) = 
+            address(auctionHouse).call{ value: bidAmount }(
+                abi.encodeWithSignature(
+                    "bid(address,uint256,uint256)", 
+                    token.contractAddress,
+                    token.tokenId,
+                    bidAmount
+                )
+            );
+        require(success, string(returnData));
+    }
+
+    /**
+     * @notice Determine whether the auction has been finalized
+     * @return TRUE if the auction has been finalized
+     */
+    function isFinalized(uint256 auctionId) external view returns (bool) {
+        Token token = auctions[_auctionId];
+        ISuperRareAuctionHouse.Auction memory auction = 
+            auctionHouse.getAuctionDetails(_contractAddress, _tokenId);
+
+        return auction.auctionType == NO_AUCTION;
+    }
+
+    /**
+     * @notice Finalize the results of the auction
+     */
+    function finalize(uint256 auctionId) external {
+        Token token = auctions[_auctionId];
+        ISuperRareAuctionHouse.Auction memory auction = 
+            auctionHouse.getAuctionDetails(_contractAddress, _tokenId);
+        
+        require(auction.auctionType != NO_AUCTION, "finalize::cant finalize auction that doesnt exist");
+        require(auction.startingBlock > 0, "finalize::cant finalize auction that hasnt started");
+        require(block.number > auction.startingBlock.add(auction.lengthOfAuction), "finalize::cant finalize currently running auction");
+
+        auctionHouse.settleAuction(token.contractAddress, token.tokenId);
+    }
+}
