0d19635c37becf2694ca80ec46fac73d36ce0d5f
Anna Carroll <anna.s.carroll@gmail.com>
2021-11-07 22:10:57 +0100

    Add Deploy test for PartyBuy

---
+++ test/buy/Deploy.test.js
@@ -0,0 +1,80 @@
+// SPDX-License-Identifier: MIT
+// ============ External Imports ============
+const { waffle } = require('hardhat');
+const { provider } = waffle;
+const { expect } = require('chai');
+// ============ Internal Imports ============
+const { deployTestContractSetup } = require('./helpers/deploy');
+const { eth } = require('../helpers/utils');
+const { PARTY_STATUS, FOURTY_EIGHT_HOURS_IN_SECONDS } = require('../helpers/constants');
+
+describe('Buy: Deploy', async () => {
+      const splitRecipient = "0x0000000000000000000000000000000000000000";
+      const splitBasisPoints = 0;
+      const reservePrice = 1;
+      const maxPrice = 10;
+      const tokenId = 95;
+      let partyBuy, partyDAOMultisig, signer, artist;
+
+      before(async () => {
+        // GET RANDOM SIGNER & ARTIST
+        [signer, artist] = provider.getWallets();
+
+        // DEPLOY PARTY BID CONTRACT
+        const contracts = await deployTestContractSetup(
+          provider,
+          artist,
+          maxPrice,
+          FOURTY_EIGHT_HOURS_IN_SECONDS,
+          splitRecipient,
+          splitBasisPoints,
+          reservePrice,
+          tokenId,
+        );
+
+        partyBuy = contracts.partyBuy;
+        partyDAOMultisig = contracts.partyDAOMultisig;
+      });
+
+      it('Party Status is Active', async () => {
+        const partyStatus = await partyBuy.partyStatus();
+        expect(partyStatus).to.equal(PARTY_STATUS.ACTIVE);
+      });
+
+      it('Version is 1', async () => {
+        const version = await partyBuy.VERSION();
+        expect(version).to.equal(1);
+      });
+
+      it('Total spent is zero', async () => {
+        const totalSpent = await partyBuy.totalSpent();
+        expect(totalSpent).to.equal(eth(0));
+      });
+
+      it('Total contributed to party is zero', async () => {
+        const totalContributedToParty = await partyBuy.totalContributedToParty();
+        expect(totalContributedToParty).to.equal(eth(0));
+      });
+
+      it('Total Contributed is zero for random account', async () => {
+        const totalContributed = await partyBuy.totalContributed(
+          signer.address,
+        );
+        expect(totalContributed).to.equal(eth(0));
+      });
+
+      it('PartyDAO Multisig is correct', async () => {
+        const multisig = await partyBuy.partyDAOMultisig();
+        expect(multisig).to.equal(partyDAOMultisig.address);
+      });
+
+      it('Name is Parrrrti', async () => {
+        const name = await partyBuy.name();
+        expect(name).to.equal("Parrrrti");
+      });
+
+      it('Symbol is PRTI', async () => {
+        const symbol = await partyBuy.symbol();
+        expect(symbol).to.equal("PRTI");
+      });
+});
---
+++ test/buy/helpers/deploy.js
@@ -0,0 +1,98 @@
+const { deploy } = require('../../helpers/deploy');
+
+async function deployTestContractSetup(
+  provider,
+  artistSigner,
+  maxPrice,
+  secondsToTimeout,
+  splitRecipient,
+  splitBasisPoints,
+  reservePrice,
+  tokenId,
+  fakeMultisig = false,
+) {
+  // Deploy WETH
+  const weth = await deploy('EtherToken');
+
+  // For other markets, deploy the test NFT Contract
+  const nftContract = await deploy('TestERC721', []);
+  // Mint token to artist
+  await nftContract.mint(artistSigner.address, tokenId);
+
+  // Deploy PartyDAO multisig
+  let partyDAOMultisig;
+  if(!fakeMultisig) {
+    partyDAOMultisig = await deploy('PayableContract');
+  } else {
+    partyDAOMultisig = artistSigner;
+  }
+
+  const tokenVaultSettings = await deploy('Settings');
+  const tokenVaultFactory = await deploy('ERC721VaultFactory', [
+    tokenVaultSettings.address,
+  ]);
+
+  // Deploy PartyBid Factory (including PartyBid Logic + Reseller Whitelist)
+  const factory = await deploy('PartyBuyFactory', [
+    partyDAOMultisig.address,
+    tokenVaultFactory.address,
+    weth.address,
+    nftContract.address,
+    tokenId,
+  ]);
+
+  // Deploy PartyBid proxy
+  await factory.startParty(
+    nftContract.address,
+    tokenId,
+    maxPrice,
+    secondsToTimeout,
+    splitRecipient,
+    splitBasisPoints,
+    'Parrrrti',
+    'PRTI',
+  );
+
+  // Get PartyBid ethers contract
+  const partyBuy = await getPartyBuyContractFromEventLogs(
+    provider,
+    factory,
+    artistSigner,
+  );
+
+  return {
+    nftContract,
+    partyBuy,
+    partyDAOMultisig,
+    weth,
+  };
+}
+
+async function getPartyBuyContractFromEventLogs(
+  provider,
+  factory,
+  artistSigner,
+) {
+  // get logs emitted from PartyBid Factory
+  const logs = await provider.getLogs({ address: factory.address });
+
+  // parse events from logs
+  const PartyBuyFactory = await ethers.getContractFactory('PartyBuyFactory');
+  const events = logs.map((log) => PartyBuyFactory.interface.parseLog(log));
+
+  // extract proxy address from PartyBuyDeployed log
+  const proxyAddress = events[0]['args'][0];
+
+  // instantiate ethers contract with PartyBid Logic interface + proxy address
+  const PartyBuy = await ethers.getContractFactory('PartyBuy');
+  const partyBuy = new ethers.Contract(
+    proxyAddress,
+    PartyBuy.interface,
+    artistSigner,
+  );
+  return partyBuy;
+}
+
+module.exports = {
+  deployTestContractSetup,
+};
